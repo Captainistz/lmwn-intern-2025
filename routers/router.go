d698024cf55cb0c66fb0606d59ba0112cddfb6b4
Captainistz <captainistz@gmail.com>
2025-01-17 21:23:19 +0700

    🌳 Add API root handler

--- routers/router.go
+++ routers/router.go
@@ -1,13 +1,19 @@
 package routers
 
 import (
+	"net/http"
+
 	"github.com/Captainistz/lmwn-intern-2025/controller"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-	r.GET("/")
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "🚀 Ready to serve.",
+		})
+	})
 	covid := r.Group("/covid")
 	{
 		covid.GET("/summary", controller.GetCovidSummary)
